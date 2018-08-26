a3511817fda3d94bbf541c4f630007e571239604
Madhan <madhan@mallow-tech.com>
2018-08-26 20:55:39 +0530

    Initial commit
    Initial commit of the project.
    
    Implemented layout.

---
+++ app/src/main/java/com/madhan/firebaseauthentication/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.madhan.firebaseauthentication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
